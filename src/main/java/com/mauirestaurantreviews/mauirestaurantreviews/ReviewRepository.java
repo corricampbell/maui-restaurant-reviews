10ca5a4cfeb3fd5472de296a6addc8c8a5edcc75
corricampbell <campbell.corri@gmail.com>
2019-02-25 17:09:05 -0500

    Maui Restaurant Reviews

--- src/main/java/com/mauirestaurantreviews/mauirestaurantreviews/ReviewRepository.java
+++ src/main/java/com/mauirestaurantreviews/mauirestaurantreviews/ReviewRepository.java
@@ -13,9 +13,9 @@ public class ReviewRepository {
 	private Map<Long, Review> reviewList = new HashMap<>();
 	
 	public ReviewRepository() {
-		Review review1 = new Review(1L, "Cool Cat Cafe", "Great restaurant and good prices!", "Review Description", "/images/CCCLogo.jpg");
-		Review review2 = new Review(2L, "The Pioneer Inn", "Best breakfast ever!", "Javascript Description", "/images/PioneerLogo.jpg");
-		Review review3 = new Review(3L, "Hula Girl", "Great way to see the sunset!", "Spring Description", "/images/HulaLogo.jpg");
+		Review review1 = new Review(1L, "Cool Cat Cafe", "Great restaurant and good prices!", "5+ Star Rating", "/images/CoolCat.png");
+		Review review2 = new Review(2L, "The Pioneer Inn", "Best breakfast ever!", "5+ Star Rating", "/images/PioneerInn.png");
+		Review review3 = new Review(3L, "Hula Girl", "Great way to see the sunset!", "5+ Star Rating", "/images/HulaGirl.png");
 		
 		reviewList.put(review1.getId(), review1);
 		reviewList.put(review2.getId(), review2);
